9d86b01eef867713d87fec21e6fd6282e5ac2a25
sidmuc21 <sidmuc21@htl-shkoder.com>
2025-04-24 17:13:00 +0200

    logout button on admin page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { enhance } from '$app/forms';
+	import { slide } from 'svelte/transition';
+
+	let { data } = $props();
+</script>
+
+<h1>ADMIN DASHBOARD - ARTICLES</h1>
+
+<a href="/admin/new">CREATE A NEW ARTICLE</a>
+
+<form action="/logout?/logout" method="POST">
+    <button class="logout" type="submit">Logout</button>
+</form>
+
+<div>
+	{#each data.articles as article (article.id)}
+		<div transition:slide>
+			<p>{article.id} - {article.author} - {article.description} - {article.votes}</p>
+
+			<form action="?/deleteArticle" method="POST" use:enhance>
+				<input type="hidden" name="id" value={article.id} />
+				<button type="submit">Delete</button>
+			</form>
+		</div>
+	{/each}
+</div>
